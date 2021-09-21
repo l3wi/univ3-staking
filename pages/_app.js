4d7e00da54bd2c6ca9771b6666339f6199085762
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2021-09-21 16:25:56 +0200

    Update Web3 Connector

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,36 @@
-import { Web3Provider } from '../contexts/useWeb3'
-import { useWallet, UseWalletProvider } from 'use-wallet'
-import { AlertProvider } from '../contexts/useAlerts'
+import { Web3Provider } from "../contexts/useWeb3"
+import { useWallet, UseWalletProvider } from "use-wallet"
+import { AlertProvider } from "../contexts/useAlerts"
 
-import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react"
 
 const theme = {
   styles: {
     global: {
-      'html, body': {
-        minHeight: '100vh',
-        fontSize: 'sm',
-        color: 'gray.600',
-        lineHeight: 'tall',
+      "html, body": {
+        minHeight: "100vh",
+        fontSize: "sm",
+        color: "gray.600",
+        lineHeight: "tall"
       },
       a: {
-        color: 'teal.500',
-      },
-    },
-  },
+        color: "teal.500"
+      }
+    }
+  }
 }
+
+const chainID = process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 1
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={extendTheme({ theme })}>
       <CSSReset />
       <UseWalletProvider
-        chainId={process.env.CHAIN_ID}
+        chainId={chainID}
         connectors={{
-          walletconnect: { rpcUrl: 'https://mainnet.eth.aragon.network/' },
-          walletlink: { url: 'https://mainnet.eth.aragon.network/' },
+          walletconnect: { rpcUrl: "https://mainnet.eth.aragon.network/" },
+          walletlink: { url: "https://mainnet.eth.aragon.network/" }
         }}
       >
         <AlertProvider>
